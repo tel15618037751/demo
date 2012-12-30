4f4835ddaf87c8c129601cc8d1c1ba0c951fb012
tel15618037751 <tel15618037751@gmail.com>
2012-12-30 20:51:31 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '6e11ff8e6f1feed651e5195792b75e4464d80d43a9cc2715ae90c677f4fb6cf1bb19c03cc21872272d15c04f5810689718401fb9ad73e48e577f7c054eadd6f3'
